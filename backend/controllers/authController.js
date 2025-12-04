5431ba01f54d07dc77ef1599692f3b1e43cb7947
Nikhil Deshmukh <nikhildeshmukhbpl@gmail.com>
2025-12-04 19:47:35 +0530

    update authcontroller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -66,7 +66,7 @@ router.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // 👇 add .select('+password')
+    //  add .select('+password')
     const user = await User.findOne({ email }).select('+password');
     if (!user) return res.status(400).json({ message: 'Invalid credentials' });
 
